71ddc3d3f484b3810acdd9914187e98b479dd1df
KrlosPK <centille095@gmail.com>
2023-04-02 17:31:11 -0500

    refactorized index.js && create .env variables

--- src/db.js
+++ src/db.js
@@ -1,9 +1,10 @@
 import { createPool } from 'mysql2/promise'
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } from './config.js'
 
 export const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'rest_api',
-  port: 3307
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  port: DB_PORT
 })
