59b81466204ad2af1c61891205fc4032b1e09949
KrlosPK <centille095@gmail.com>
2023-04-02 17:05:24 -0500

    handle app errors

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -1,51 +1,71 @@
 import { pool } from '../db.js'
 
 export const getEmployees = async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM empleados')
+  try {
+    const [rows] = await pool.query('SELECT * FROM empleados')
 
-  res.json(rows)
+    res.json(rows)
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al obtener los empleados', error })
+  }
 }
 
 export const getEmployeeById = async (req, res) => {
-  const { id } = req.params
+  try {
+    const { id } = req.params
 
-  const [rows] = await pool.query('SELECT * FROM empleados WHERE id = ?', [id])
+    const [rows] = await pool.query('SELECT * FROM empleados WHERE id = ?', [id])
 
-  if (rows.length <= 0) return res.status(404).json({ message: 'Empleado no encontrado' })
+    if (rows.length <= 0) return res.status(404).json({ message: 'Empleado no encontrado' })
 
-  res.send(rows[0])
+    res.send(rows[0])
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al obtener el empleado', error })
+  }
 }
 
 export const createEmployee = async (req, res) => {
-  const { nombre, salario } = req.body
+  try {
+    const { nombre, salario } = req.body
 
-  const [rows] = await pool.query('INSERT INTO empleados (nombre, salario) VALUES (?, ?)', [nombre, salario])
-  res.send({ rows })
+    const [rows] = await pool.query('INSERT INTO empleados (nombre, salario) VALUES (?, ?)', [nombre, salario])
+    res.send({ rows })
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al crear el empleado', error })
+  }
 }
 
 export const updateEmployee = async (req, res) => {
-  const { id } = req.params
-  const { nombre, salario } = req.body
+  try {
+    const { id } = req.params
+    const { nombre, salario } = req.body
 
-  const [result] = await pool.query('UPDATE empleados SET nombre = IFNULL(?, nombre), salario = IFNULL(?, salario) WHERE id = ?', [
-    nombre,
-    salario,
-    id
-  ])
+    const [result] = await pool.query('UPDATE empleados SET nombre = IFNULL(?, nombre), salario = IFNULL(?, salario) WHERE id = ?', [
+      nombre,
+      salario,
+      id
+    ])
 
-  if (result.affectedRows <= 0) return res.status(404).json({ message: 'Empleado no encontrado' })
+    if (result.affectedRows <= 0) return res.status(404).json({ message: 'Empleado no encontrado' })
 
-  const [rows] = await pool.query('SELECT * FROM empleados WHERE id = ?', [id])
+    const [rows] = await pool.query('SELECT * FROM empleados WHERE id = ?', [id])
 
-  res.json(rows[0])
+    res.json(rows[0])
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al actualizar el empleado', error })
+  }
 }
 
 export const deleteEmployee = async (req, res) => {
-  const { id } = req.params
+  try {
+    const { id } = req.params
 
-  const [result] = await pool.query('DELETE FROM empleados WHERE id = ?', [id])
+    const [result] = await pool.query('DELETE FROM empleados WHERE id = ?', [id])
 
-  if (result.affectedRows <= 0) return res.status(404).json({ message: 'Empleado no encontrado' })
+    if (result.affectedRows <= 0) return res.status(404).json({ message: 'Empleado no encontrado' })
 
-  res.sendStatus(204)
+    res.sendStatus(204)
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al eliminar el empleado', error })
+  }
 }
--- src/controllers/publicaciones.controller.js
+++ src/controllers/publicaciones.controller.js
@@ -1,56 +1,76 @@
 import { pool } from '../db.js'
 
 export const getPublications = async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM publicaciones WHERE estado_publicacion = "activa"')
+  try {
+    const [rows] = await pool.query('SELECT * FROM publicaciones WHERE estado_publicacion = "activa"')
 
-  res.send(rows)
+    res.send(rows)
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al obtener las publicaciones', error })
+  }
 }
 
 export const getPublicationById = async (req, res) => {
-  const { id } = req.params
+  try {
+    const { id } = req.params
 
-  const [rows] = await pool.query('SELECT * FROM publicaciones WHERE id_publicacion = ?', [id])
-  if (rows.length <= 0) return res.status(404).json({ message: 'Publicación no encontrada' })
+    const [rows] = await pool.query('SELECT * FROM publicaciones WHERE id_publicacion = ?', [id])
+    if (rows.length <= 0) return res.status(404).json({ message: 'Publicación no encontrada' })
 
-  res.json(rows[0])
+    res.json(rows[0])
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al obtener la publicación', error })
+  }
 }
 
 export const createPublication = async (req, res) => {
-  const { descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion } = req.body
+  try {
+    const { descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion } = req.body
 
-  const [rows] = await pool.query(
-    'INSERT INTO publicaciones (descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion, estado_publicacion) VALUES (?, ?, ?, ?, "activa")',
-    [descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion]
-  )
-  res.send({ rows })
+    const [rows] = await pool.query(
+      'INSERT INTO publicaciones (descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion, estado_publicacion) VALUES (?, ?, ?, ?, "activa")',
+      [descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion]
+    )
+    res.send({ rows })
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al crear la publicación', error })
+  }
 }
 
 export const updatePublication = async (req, res) => {
-  const { id } = req.params
-  const { descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion } = req.body
+  try {
+    const { id } = req.params
+    const { descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion } = req.body
 
-  const [result] = await pool.query(
-    'UPDATE publicaciones SET descripcion_publicacion = IFNULL(?, descripcion_publicacion), galeria_publicacion = IFNULL(?, galeria_publicacion), fecha_creacion = IFNULL(?, fecha_creacion), fecha_modificacion = IFNULL(?, fecha_modificacion) WHERE id_publicacion = ?',
-    [descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion, id]
-  )
-  if (result.affectedRows <= 0)
-    return res.status(404).json({
-      message: 'Publicación no encontrada'
-    })
+    const [result] = await pool.query(
+      'UPDATE publicaciones SET descripcion_publicacion = IFNULL(?, descripcion_publicacion), galeria_publicacion = IFNULL(?, galeria_publicacion), fecha_creacion = IFNULL(?, fecha_creacion), fecha_modificacion = IFNULL(?, fecha_modificacion) WHERE id_publicacion = ?',
+      [descripcion_publicacion, galeria_publicacion, fecha_creacion, fecha_modificacion, id]
+    )
+    if (result.affectedRows <= 0)
+      return res.status(404).json({
+        message: 'Publicación no encontrada'
+      })
 
-  const [rows] = await pool.query('SELECT * FROM publicaciones WHERE id_publicacion = ?', [id])
+    const [rows] = await pool.query('SELECT * FROM publicaciones WHERE id_publicacion = ?', [id])
 
-  res.json(rows[0])
+    res.json(rows[0])
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al actualizar la publicación', error })
+  }
 }
 
 export const deletePublication = async (req, res) => {
-  const { id } = req.params
+  try {
+    const { id } = req.params
 
-  const [result] = await pool.query('DELETE FROM publicaciones WHERE id_publicacion = ?', [id])
-  if (result.affectedRows <= 0)
-    return res.status(404).json({
-      message: 'Publicación no encontrada'
-    })
+    const [result] = await pool.query('DELETE FROM publicaciones WHERE id_publicacion = ?', [id])
+    if (result.affectedRows <= 0)
+      return res.status(404).json({
+        message: 'Publicación no encontrada'
+      })
 
-  res.sendStatus(204)
+    res.sendStatus(204)
+  } catch (error) {
+    return res.status(500).json({ message: 'Error al eliminar la publicación', error })
+  }
 }
