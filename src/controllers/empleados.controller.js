d30225f505e9b54b3467cc268b7f0311ab06385b
KrlosPK <centille095@gmail.com>
2023-04-02 16:23:52 -0500

    fix employess id bug

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -9,7 +9,7 @@ export const getEmployees = async (req, res) => {
 export const getEmployeeById = async (req, res) => {
   const { id } = req.params
 
-  const [rows] = await pool.query('SELECT * FROM empleados WHERE id_empleado = ?', [id])
+  const [rows] = await pool.query('SELECT * FROM empleados WHERE id = ?', [id])
 
   if (rows.length <= 0) return res.status(404).json({ message: 'Empleado no encontrado' })
 
@@ -31,7 +31,7 @@ export const updateEmployee = async (req, res) => {
   const { id } = req.params
   const { nombre, salario } = req.body
 
-  const [result] = await pool.query('UPDATE empleados SET nombre = IFNULL(?, nombre), salario = IFNULL(?, salario) WHERE id_empleado = ?', [
+  const [result] = await pool.query('UPDATE empleados SET nombre = IFNULL(?, nombre), salario = IFNULL(?, salario) WHERE id = ?', [
     nombre,
     salario,
     id
@@ -45,7 +45,7 @@ export const updateEmployee = async (req, res) => {
 export const deleteEmployee = async (req, res) => {
   const { id } = req.params
 
-  const [result] = await pool.query('DELETE FROM empleados WHERE id_empleado = ?', [id])
+  const [result] = await pool.query('DELETE FROM empleados WHERE id = ?', [id])
 
   if (result.affectedRows <= 0) return res.status(404).json({ message: 'Empleado no encontrado' })
 
